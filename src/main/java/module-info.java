7fcbd30c8a366855fe8a446133159e2f8f497e79
Vianny Cruz <vmcc0001@ce.pucmm.edu.do>
2024-07-09 11:45:38 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.proyectofinal_icc303 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.proyectofinal_icc303 to javafx.fxml;
+    exports com.example.proyectofinal_icc303;
+}
